d95555b20c91d226d9879a5a6e52d50f53cea788
HannahBagtasos <hannahvistabagtasos@gmail.com>
2022-07-31 23:12:29 +0100

    css issue

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const { application } = require("express");
 const express = require("express");
-
+const path= require('path');
 const server = express();
 
 server.get("/",function (req,res){
     res.sendFile(__dirname + "/GenderAPI.html");
 });
+
+server.use(express.static('public'));
 
 server.listen(3000,function () {
     console.log("http://localhost:3000");
