5ecfb6d658a82d94898f18fea7b6578d2c6391cb
HannahBagtasos <hannahvistabagtasos@gmail.com>
2022-09-05 23:45:53 +0100

    Multiple Name and Email Commit
    Users can now paste a JSON array for multiple names and emails.

--- multigenderapi.js
+++ multigenderapi.js
@@ -1,4 +1,124 @@
+var apiEndpoint = "https://gender-api.com/v2/gender"
+var urlParams = new URLSearchParams(window.location.search);
+var xhr = new XMLHttpRequest();
+var form = document.getElementById("form");
+var gender2m = 0;
+var gender2f = 0;
+var gender2u = 0;
+                                   
+var result2 = document.getElementById("result2");
+var gender2 = document.getElementById("gender2");
+var authToken = urlParams.get("authToken");
+var json = document.getElementById("json");
+json.value = urlParams.get("json");
+//document.getElementById("apiEndpoint").innerHTML = apiEndpoint;
+//document.getElementById("header").innerHTML = "Authorization: Bearer " + authToken;
 
+form.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+  var requestPayload = JSON.parse(json.value);
+
+  xhr.open("POST", apiEndpoint);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer " + "02dd39f7e003776aa4ba778a0df38c002a051d7086b8fae1c6915ac9f756d55a");
+
+   xhr.onload = function() {
+    if (xhr.status === 200) {
+      //result2.textContent = xhr.responseText;
+      //result2.style.display = "inline-block";
+     //Gender counter for the pie chart
+     var response2 = JSON.parse(xhr.responseText) 
+     //console.log(response2.gender);
+    for(person in response2){
+     if(response2[person].gender === 'female'){
+        gender2f++;
+        console.log("There is this many female names generated: " + gender2f);
+    }
+    else if (response2[person].gender === 'male'){
+        gender2m++;
+        console.log("There is this many male names generated: " + gender2m);
+    
+    }
+    else{
+        gender2u++;
+        console.log("There is this many unknown names generated: " + gender2u );
+
+    } 
+    
+    //Pie chart
+
+        var xValues = ["Male", "Female"];
+        var yValues = [gender2m, gender2f];
+        
+        var barColors = [
+          "#b91d47",
+          "#00aba9"
+        ];
+        
+        new Chart("myChart", {
+          type: "pie",
+          data: {
+            labels: xValues,
+            datasets: [{
+              backgroundColor: barColors,
+              data: yValues
+            }]
+        }
+        });
+    }
+    
+
+     
+        
+    }
+   // console.log(xhr.responseText);
+
+
+  };
+  xhr.send(JSON.stringify(requestPayload));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let multibutton = document.querySelector(".button")
 let multi_name_sanitized = document.querySelector(".name")
 let multigender = document.querySelector(".gender")
@@ -110,6 +230,6 @@ var multigenderu = 0;
     document.getElementById('file').addEventListener('change', onChange);
 
    
-   
+   */
     
 
