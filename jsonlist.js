155557ca56cc9f043ff52c3ea0cbe4416003fe58
HannahBagtasos <hannahvistabagtasos@gmail.com>
2022-09-21 14:45:18 +0100

    Finalised JSON upload with pie chart

--- jsonlist.js
+++ jsonlist.js
@@ -19,12 +19,49 @@ function readFile(event){
     
     xhr.onload = function() {
         if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText)
-            console.log(response)
+            var response2 = JSON.parse(xhr.responseText)
+            console.log(response2)
+            for(person in response2){
+                if(response2[person].gender === 'female'){
+                   gender2f++;
+                   console.log("There is this many female names generated: " + gender2f);
+               }
+               else if (response2[person].gender === 'male'){
+                   gender2m++;
+                   console.log("There is this many male names generated: " + gender2m);
+               
+               }
+               else{
+                   gender2u++;
+                   console.log("There is this many unknown names generated: " + gender2u );
+           
+               } 
+            }
+            
+            //Pie chart
+
+        var xValues = ["Male", "Female"];
+        var yValues = [gender2m, gender2f];
+        
+        var barColors = [
+          "#b91d47",
+          "#00aba9"
+        ];
+        
+        new Chart("myChart", {
+          type: "pie",
+          data: {
+            labels: xValues,
+            datasets: [{
+              backgroundColor: barColors,
+              data: yValues
+            }]
+        }
+        });
+           
         }
     }
     xhr.send(JSON.stringify(object));
 }
 document.getElementById('selectFiles').addEventListener('change', jsonReader);
  
-  
